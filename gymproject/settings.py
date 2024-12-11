e526b2faefdd8e62765c69c468da79f228d4f0a3
Rohail <rohail230@gmail.com>
2024-12-11 19:49:45 +0530

    modified:   gymproject/__pycache__/settings.cpython-312.pyc 	modified:   gymproject/settings.py
    commit

--- gymproject/settings.py
+++ gymproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8oai95!6vx%i33a&0_*n63-$0#x8ltc+13@9(kf9)cwo)vs957'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['Rohailkhan.pythonanywhere.com']
 
 
 # Application definition
